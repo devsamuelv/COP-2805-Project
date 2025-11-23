72d94b86b0bd166b43e05c9535b9a5d9176447f5
uniMatrix7 <hcantres@icoud.com>
2025-11-22 21:20:27 -0500

    * TPC_DBCommencing.java (NEW)
    	* Creates JavaDB database of non exist
    	* Runs SQL from TPC-H_DDl-1.sql
    	* Make sure table exist before using API
    	* Logs setup steps
    
    * Main.java (Update)
    
    	* Create a TPC_DBCommencing starter = new TPC_DBCommencing();
    	* Call starter.initDatabase();
    	* Move creation of TPC_DBAPI into main after DB init
    	* Remove static initialization of the API

--- src/edu/easternflorida/villegas/Main.java
+++ src/edu/easternflorida/villegas/Main.java
@@ -1,25 +1,47 @@
 package edu.easternflorida.villegas;
 
 import java.math.BigDecimal;
+import edu.easternflorida.cantres.TPC_DBCommencing;
 import java.util.HashMap;
-
 import edu.easternflorida.villegas.interfaces.Part;
 
 public class Main {
-  static TPC_DBAPI api = new TPC_DBAPI();
 
-  public static void main(String[] args) {
-    HashMap<Integer, Part> parts = api.readAllParts();
-    Part magicPart = new Part(1000000000, "yeaa", "yeaa", "yeaa", "yeaa", 0, "yeaa", BigDecimal.ONE, "yeaa");
+public static void main(String[] args) {
+ try {
+   TPC_DBCommencing starter = new TPC_DBCommencing();
+   starter.initDatabase();   // creates DB + tables if needed
 
-    System.out.println("has record " + parts.containsKey(magicPart.getP_PARTKEY()));
+   TPC_DBAPI api = new TPC_DBAPI();
 
-    try {
-      api.insertPart(magicPart);
-    } catch (Exception err) {
-      err.printStackTrace();
-    }
+   HashMap<Integer, Part> parts = api.readAllParts();
 
-    System.out.println("has record " + parts.containsKey(magicPart.getP_PARTKEY()));
-  }
+   Part magicPart = new Part(
+       1000000000,
+       "yeaa",
+       "yeaa",
+       "yeaa",
+       "yeaa",
+       0,
+       "yeaa",
+       BigDecimal.ONE,
+       "yeaa"
+   );
+
+   System.out.println("has record before insert: " + parts.containsKey(magicPart.getP_PARTKEY()));
+
+   try {
+     api.insertPart(magicPart);
+   } catch (Exception err) {
+     err.printStackTrace();
+   }
+
+   parts = api.readAllParts();
+   System.out.println("has record after insert: " + parts.containsKey(magicPart.getP_PARTKEY()));
+
+ } catch (Exception e) {
+   e.printStackTrace();
+ }
 }
+}
+
